933421991aa6063553aad327545cb05fa00c2a7b
蛮吉 <hanabi.almost@gmail.com>
2026-06-18 12:55:44 +0800

    chore: 锁定 Gradle Daemon 使用 Java 25
    - 添加 Foojay toolchain resolver 配置
    
    - 提交 Java 25 Adoptium daemon criteria

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,7 @@ dependencyResolutionManagement {
 
 plugins {
     id("com.highcapable.gropify") version "1.0.2"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 gropify {
